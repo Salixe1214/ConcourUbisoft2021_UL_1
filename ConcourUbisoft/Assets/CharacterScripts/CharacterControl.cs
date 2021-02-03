d68f76306fd21ceb87bd61a6e28879dee9224eab
mthb54 <42650980+mthb54@users.noreply.github.com>
2021-02-03 11:17:48 -0500

    PlayerBasicMovement

---
+++ ConcourUbisoft/Assets/CharacterScripts/CharacterControl.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float playerMovementSpeed = 1f; 
+    private Rigidbody playerBody;
+    private Vector3 inputVector;
+    
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVector = new Vector3(Input.GetAxis("Horizontal") * playerMovementSpeed,playerBody.velocity.y,Input.GetAxis("Vertical")*playerMovementSpeed);
+        transform.LookAt(playerBody.position + new Vector3(inputVector.x,0,inputVector.z));
+    }
+
+    private void FixedUpdate()
+    {
+        playerBody.velocity = inputVector;
+    }
+}
