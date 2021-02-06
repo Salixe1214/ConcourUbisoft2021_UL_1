4267a0f0a72cb3495a290722968a4cf3295cc3b4
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2021-02-06 14:52:14 -0500

    Implementation du voiceChat

---
+++ ConcourUbisoft/Assets/NetworkVoiceManager.cs
@@ -0,0 +1,45 @@
+/*
+ * Source: https://github.com/silidragos/TCG/blob/master/Assets/Scripts/Networking/NetworkVoiceManager.cs
+ */
+
+using Photon.Pun;
+using Photon.Voice.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+[RequireComponent(typeof(VoiceConnection))]
+public class NetworkVoiceManager : MonoBehaviour
+{
+    public Transform RemoteVoiceParent;
+
+    private VoiceConnection voiceConnection;
+    
+    void Awake()
+    {
+        voiceConnection = GetComponent<VoiceConnection>();
+    }
+
+    private void OnEnable()
+    {
+        voiceConnection.SpeakerLinked += this.OnSpeakerCreated;
+    }
+    
+    private void OnDisable()
+    {
+        voiceConnection.SpeakerLinked -= this.OnSpeakerCreated;
+    }
+
+    private void OnSpeakerCreated(Speaker pSpeaker)
+    {
+        pSpeaker.transform.SetParent(RemoteVoiceParent);
+        pSpeaker.OnRemoteVoiceRemoveAction += OnRemoteVoiceRemove;
+    }
+
+    private void OnRemoteVoiceRemove(Speaker pSpeaker)
+    {
+        if(pSpeaker != null)
+            Destroy(pSpeaker.gameObject);
+    }
+}
