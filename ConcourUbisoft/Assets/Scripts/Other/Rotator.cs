773a443f22369ff2549c6d2eefde66aa2957a077
Gabriel Bouchard <gaboutch@outlook.com>
2021-02-09 15:34:32 -0500

    Update Rotator.cs

--- ConcourUbisoft/Assets/Scripts/Other/Rotator.cs
+++ ConcourUbisoft/Assets/Scripts/Other/Rotator.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 public class rotator : MonoBehaviour
 {
     [SerializeField] private float speed=25;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up*Time.deltaTime*speed);
