675491a6db932dc5b6b1f334b5d34bcf7678a930
Gabriel Bouchard <gaboutch@outlook.com>
2021-02-16 14:14:59 -0500

    sounds and magnet

--- ConcourUbisoft/Assets/Scripts/RoboticArm/ArmController.cs
+++ ConcourUbisoft/Assets/Scripts/RoboticArm/ArmController.cs
@@ -2,32 +2,18 @@
 
 namespace Arm
 {
-    enum GrabState
-    {
-        NONE,
-        MOVE_TO_TARGET,
-        GRABBING,
-        GRABBED,
-        RESET_POSITION,
-    }
-
     public class ArmController : MonoBehaviour
     {
-        [SerializeField] private bool controlled;
-        [SerializeField] private Transform head;
+        [SerializeField] private Controllable controllable;
         [SerializeField] private IKSolver armIKSolver;
-        [SerializeField] private Transform grabPoint;
         [SerializeField] private Transform armRotationRoot;
         [SerializeField] float controlSpeed = 3f;
-        [SerializeField] private float grabSpeed = 3f;
-        [SerializeField] private float headRotation = 180;
         private Transform armTarget = null;
-        private Pickable currentPickable = null;
-        private Vector3 currentPickableHitPosition;
+
+
         private float maxRange;
         private float minRange;
-        private float targetStartY;
-        private GrabState grabState = GrabState.NONE;
+
 
         public float ControlSpeed => controlSpeed;
 
@@ -37,23 +23,11 @@ namespace Arm
             maxRange = armIKSolver.TotalLength;
             minRange = armIKSolver.TotalLength / 4;
             armTarget = armIKSolver.Target;
-            targetStartY = armTarget.transform.localPosition.y;
-            head.transform.Rotate(new Vector3(0, 0, 1), headRotation - head.transform.eulerAngles.z);
         }
 
         void Update()
         {
-            if (controlled)
-            {
-                UpdateCurrentPickable();
-                UpdateArm();
-                UpdateHead();
-            }
-        }
-
-        private void UpdateArm()
-        {
-            if (grabState == GrabState.NONE || grabState == GrabState.GRABBED)
+            if (controllable.IsControlled)
             {
                 Vector3 translation = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
                 armTarget.transform.Translate(Time.deltaTime * controlSpeed * translation);
@@ -82,123 +56,6 @@ namespace Arm
             direction.Normalize();
             float rotationY = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             armRotationRoot.rotation = Quaternion.Euler(0f, rotationY - 90, 0f);
-        }
-
-        private void UpdateHead()
-        {
-            head.transform.Rotate(new Vector3(0, 0, 1), headRotation - head.transform.eulerAngles.z);
-
-            switch (grabState)
-            {
-                case GrabState.NONE:
-                    if ((Input.GetButtonDown("Grab") ||
-                         Input.GetButtonDown("GrabControllerXBO") ||
-                         Input.GetButtonDown("GrabControllerPS")) && currentPickable)
-                        grabState = GrabState.MOVE_TO_TARGET;
-                    break;
-                case GrabState.MOVE_TO_TARGET:
-                    MoveToCurrentPickable();
-                    break;
-                case GrabState.GRABBING:
-
-
-                    break;
-                case GrabState.RESET_POSITION:
-                    ResetPosition();
-                    break;
-                case GrabState.GRABBED:
-                    if ((Input.GetButtonDown("Grab") ||
-                         Input.GetButtonDown("GrabControllerXBO") ||
-                         Input.GetButtonDown("GrabControllerPS")))
-                        Release();
-                    break;
-                default:
-                    grabState = GrabState.NONE;
-                    break;
-            }
-        }
-
-        private void Release()
-        {
-            currentPickable.transform.SetParent(null);
-            grabState = GrabState.NONE;
-            currentPickable.OnRelease();
-            currentPickable = null;
-        }
-
-        private void MoveToCurrentPickable()
-        {
-            if (currentPickable)
-            {
-                if (currentPickable.Contains(grabPoint.position))
-                {
-                    grabState = GrabState.RESET_POSITION;
-                    currentPickable.transform.SetParent(grabPoint.transform);
-                    currentPickable.OnGrab();
-                }
-
-                float speed = SpeedFunction(0.2f, 0.8f,
-                    (armTarget.transform.localPosition.y - currentPickableHitPosition.y) /
-                    (targetStartY - currentPickableHitPosition.y));
-                armTarget.transform.Translate(speed * grabSpeed * Time.deltaTime * Vector3.down);
-            }
-            else
-            {
-                grabState = GrabState.RESET_POSITION;
-            }
-        }
-
-        private float SpeedFunction(float a, float b, float x)
-        {
-            if (x < a)
-            {
-                return Mathf.SmoothStep(0.1f, 1, x / a);
-            }
-
-            if (x < b)
-            {
-                return 1.0f;
-            }
-
-            return Mathf.SmoothStep(1, 0.1f, (x - b) / a);
-        }
-
-        private void ResetPosition()
-        {
-            float speed = SpeedFunction(0.2f, 0.8f,
-                (armTarget.transform.localPosition.y - currentPickableHitPosition.y) /
-                (targetStartY - currentPickableHitPosition.y));
-            armTarget.transform.Translate(speed * grabSpeed * Time.deltaTime * Vector3.up);
-            if (armTarget.transform.localPosition.y >= targetStartY)
-            {
-                armTarget.transform.localPosition = new Vector3(
-                    armTarget.transform.localPosition.x,
-                    targetStartY,
-                    armTarget.transform.localPosition.z);
-                if (currentPickable != null)
-                    grabState = GrabState.GRABBED;
-                else
-                    grabState = GrabState.NONE;
-            }
-        }
-
-        private void UpdateCurrentPickable()
-        {
-            RaycastHit hit;
-            Vector3 headDirection = head.transform.up;
-            if (Physics.Raycast(grabPoint.position, headDirection, out hit, Mathf.Infinity))
-            {
-                currentPickable = hit.transform.GetComponent<Pickable>();
-                if (currentPickable)
-                {
-                    currentPickable.OnHover();
-                    Debug.DrawRay(grabPoint.position, headDirection * hit.distance, Color.green);
-                }
-                else
-                {
-                    currentPickable = null;
-                }
-            }
         }
     }
 }
--- ConcourUbisoft/Assets/Scripts/RoboticArm/Pickable.cs
+++ ConcourUbisoft/Assets/Scripts/RoboticArm/Pickable.cs
@@ -1,4 +1,4 @@
-
+using System;
 using UnityEngine;
 
 namespace Arm
@@ -6,18 +6,26 @@ namespace Arm
     [RequireComponent(typeof(Rigidbody))]
     [RequireComponent(typeof(Collider))]
     [RequireComponent(typeof(Outline))]
+    [RequireComponent(typeof(AudioSource))]
     public class Pickable : MonoBehaviour
     {
+        [SerializeField] private AudioClip magnetCollisionSound;
+        [SerializeField] private AudioClip conveyorCollisionSound;
+        [SerializeField] private AudioClip groundCollisionSound;
+        private AudioSource audioSource;
         private Rigidbody rigidbody;
+
         private Collider collider;
         private Outline outline;
         private bool hovered;
+        public Rigidbody RB => rigidbody;
 
         private void Start()
         {
             rigidbody = GetComponent<Rigidbody>();
             collider = GetComponent<Collider>();
             outline = GetComponent<Outline>();
+            audioSource = GetComponent<AudioSource>();
             outline.enabled = false;
         }
 
@@ -43,6 +51,15 @@ namespace Arm
         public void OnHover()
         {
             hovered = true;
+        }
+
+        public void OnCollisionEnter(Collision other)
+        {
+            if (other.transform.CompareTag("Magnet"))
+            {
+                audioSource.clip = magnetCollisionSound;
+                audioSource.Play();
+            }
         }
 
         public void OnRelease()
