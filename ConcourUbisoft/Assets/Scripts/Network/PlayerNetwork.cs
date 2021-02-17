f2b3a89ace8c755463fb398eb0a98ea2e561475a
Gabriel Bouchard <gaboutch@outlook.com>
2021-02-17 13:14:42 -0500

    test arm networked movement

--- ConcourUbisoft/Assets/Scripts/Network/PlayerNetwork.cs
+++ ConcourUbisoft/Assets/Scripts/Network/PlayerNetwork.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Arm;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -10,14 +11,20 @@ public class PlayerNetwork : MonoBehaviourPun, IPunObservable
 {
     public GameController.Role PlayerRole { get; set; }
     public string Name { set; get; }
-    public string Id { get { return photonView.Owner.UserId; } }
+
+    public string Id
+    {
+        get { return photonView.Owner.UserId; }
+    }
 
     private new PhotonView photonView = null;
     private NetworkController networkController = null;
     private GameController gameController = null;
     private GameObject playerA = null;
+    private ArmController[] Arms;
 
     #region Unity Callbacks
+
     private void Awake()
     {
         photonView = GetComponent<PhotonView>();
@@ -26,10 +33,12 @@ public class PlayerNetwork : MonoBehaviourPun, IPunObservable
 
         Name = $"Player {(photonView.Owner.IsMasterClient ? "1" : "2")}";
     }
+
     private void Start()
     {
         networkController.InvokePlayerNetworkInstantiate();
     }
+
     private void OnEnable()
     {
         gameController.OnLoadGameEvent += OnLoadGameEvent;
@@ -45,12 +54,14 @@ public class PlayerNetwork : MonoBehaviourPun, IPunObservable
     }
 
     #endregion
+
     #region Photon Callbacks
+
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
         {
-            stream.SendNext((int)PlayerRole);
+            stream.SendNext((int) PlayerRole);
             stream.SendNext(Name);
 
             if (gameController.IsGameStart && PlayerRole == GameController.Role.SecurityGuard)
@@ -58,31 +69,55 @@ public class PlayerNetwork : MonoBehaviourPun, IPunObservable
                 stream.SendNext(playerA.transform.position);
                 stream.SendNext(playerA.transform.rotation);
             }
+
+            if (gameController.IsGameStart && PlayerRole == GameController.Role.Technician)
+            {
+                for (int i = 0; i < Arms.Length; i++)
+                {
+                    stream.SendNext(Arms[i].transform.position);
+                    stream.SendNext(Arms[i].transform.rotation);
+                }
+            }
         }
         else
         {
-            this.PlayerRole = (GameController.Role)(int)stream.ReceiveNext();
-            this.Name = (string)stream.ReceiveNext();
+            this.PlayerRole = (GameController.Role) (int) stream.ReceiveNext();
+            this.Name = (string) stream.ReceiveNext();
 
             if (gameController.IsGameStart && PlayerRole == GameController.Role.SecurityGuard)
             {
-                playerA.transform.position = (Vector3)stream.ReceiveNext();
-                playerA.transform.rotation = (Quaternion)stream.ReceiveNext();
+                playerA.transform.position = (Vector3) stream.ReceiveNext();
+                playerA.transform.rotation = (Quaternion) stream.ReceiveNext();
+            }
+            else if (gameController.IsGameStart && PlayerRole == GameController.Role.Technician)
+            {
+                for (int i = 0; i < Arms.Length; i++)
+                {
+                    Arms[i].transform.position = (Vector3) stream.ReceiveNext();
+                    Arms[i].transform.rotation = (Quaternion) stream.ReceiveNext();
+                }
             }
         }
     }
+
     #endregion
+
     #region Public Functions
+
     public bool IsMasterClient()
     {
         return photonView.Owner.IsMasterClient;
     }
+
     public bool IsMine()
     {
         return photonView.IsMine;
     }
+
     #endregion
+
     #region RPC Functions
+
     [PunRPC]
     private void StartGame()
     {
@@ -91,24 +126,33 @@ public class PlayerNetwork : MonoBehaviourPun, IPunObservable
             gameController.StartGame(networkController.GetLocalRole());
         }
     }
+
     [PunRPC]
     private void UnlockDoor(object[] parameters)
     {
-        DoorsScript doorsScript = FindObjectsOfType<DoorsScript>().Where(x => x.DoorId == (int)parameters[0]).FirstOrDefault();
+        DoorsScript doorsScript = FindObjectsOfType<DoorsScript>().Where(x => x.DoorId == (int) parameters[0])
+            .FirstOrDefault();
         if (doorsScript != null)
         {
             doorsScript.UnlockDoor();
         }
     }
+
     #endregion
+
     #region Private Functions
+
     private void OnLoadGameEvent()
     {
         photonView.RPC("StartGame", RpcTarget.Others);
     }
+
     private void OnFinishLoadGameEvent()
     {
         playerA = GameObject.FindGameObjectWithTag("PlayerGuard");
+        Arms = new ArmController[1];
+        Arms[0] = GameObject.FindGameObjectWithTag("Arm1").GetComponent<ArmController>();
+        //Arms[1] = GameObject.FindGameObjectWithTag("Arm2").GetComponent<ArmController>();
 
         if (IsMine())
         {
@@ -122,7 +166,7 @@ public class PlayerNetwork : MonoBehaviourPun, IPunObservable
 
     private void OnDoorUnlockEvent(DoorsScript doorsScript)
     {
-        object[] parameters = new object[] { doorsScript.DoorId };
+        object[] parameters = new object[] {doorsScript.DoorId};
         photonView.RPC("UnlockDoor", RpcTarget.Others, parameters as object);
     }
 
