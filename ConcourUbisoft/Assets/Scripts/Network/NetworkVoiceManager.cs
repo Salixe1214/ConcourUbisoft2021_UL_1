ab486aba4d505b9b1b96c3c1be0b4be8c9b2970d
mthb54 <42650980+mthb54@users.noreply.github.com>
2021-02-21 19:31:49 -0500

    Voice Chat Mapping to controllers
    R2 PS
    RT XBO

--- ConcourUbisoft/Assets/Scripts/Network/NetworkVoiceManager.cs
+++ ConcourUbisoft/Assets/Scripts/Network/NetworkVoiceManager.cs
@@ -1,5 +1,6 @@
 using Photon.Voice.Unity;
 using UnityEngine;
+using System.Linq;
 
 [RequireComponent(typeof(VoiceConnection))]
 public class NetworkVoiceManager : MonoBehaviour
@@ -14,11 +15,37 @@ public class NetworkVoiceManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        string[] joysticks = Input.GetJoystickNames();
+
+        if (joysticks.Contains("Controller (Xbox One For Windows)"))
+        {
+            if (Input.GetAxis("VoiceXBO") >0.2)
+            {
+                recorder.IsRecording = true;
+            }
+            else
+            {
+                recorder.IsRecording = false;
+            }
+        }
+        else if (joysticks.Contains("Wireless Controller"))
+        {
+            if (Input.GetAxis("VoicePS") > -0.9)
+            {
+                recorder.IsRecording = true;
+            }
+            else
+            {
+                recorder.IsRecording = false;
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             recorder.IsRecording = true;
         }
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             recorder.IsRecording = false;
         }
