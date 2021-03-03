0e7027ac5b01e36eea8c0173e24af633ec3fd1c5
ClaXXX <claire.bouttes@gmail.com>
2021-03-02 21:36:10 -0500

    (update) Sprite example

--- ConcourUbisoft/Assets/Scripts/TechSupport/Informations/InformationsSystem.cs
+++ ConcourUbisoft/Assets/Scripts/TechSupport/Informations/InformationsSystem.cs
@@ -22,14 +22,16 @@ namespace TechSupport.Informations
         [Header("List")] 
         private ImageLayout _imageLayout;
         [SerializeField] private List<Sprite> imagesList;
+        [SerializeField] private Vector2 sizeList;
 
         [Header("Accordion")] 
         private Accordion _accordion;
         [SerializeField] private List<InformationItem> items;
-        [SerializeField] private Vector3 position;
-        [SerializeField] private Vector2 size;
         [SerializeField] private Sprite background;
         [SerializeField] private Sprite front;
+
+        private RectTransform _listRectTransform;
+        private RectTransform _accordionRectTransform;
 
         private void Awake()
         {
@@ -37,24 +39,33 @@ namespace TechSupport.Informations
             CreateAccordion();
         }
 
+        private void Update()
+        {
+            _imageLayout.transform.position = new Vector3 (Screen.width * 0.5f, Screen.height * 0.95f, 0);
+            _accordion.transform.position = new Vector3(Screen.width * 0.85f, Screen.height * 0.5f);
+            _accordionRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Screen.width * 0.15f);
+        }
+
+        private void SetSize(RectTransform rectTransform, Vector2 size)
+        {
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size.x);
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size.y);
+        }
         private void CreateList()
         {
             _imageLayout = new GameObject().AddComponent<ImageLayout>();
-            
-            _imageLayout.GetComponent<RectTransform>().SetParent(transform);
-            _imageLayout.transform.position = new Vector3 (Screen.width * 0.5f, Screen.height * 0.9f, 0);
+            _listRectTransform = _imageLayout.GetComponent<RectTransform>();
+            _listRectTransform.SetParent(transform);
+            SetSize(_listRectTransform, sizeList);
             _imageLayout.CreateLayout(imagesList);
         }
 
         private void CreateAccordion()
         {
             _accordion = new GameObject().AddComponent<Accordion>();
-
-            _accordion.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size.x);
-//            _accordion.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size.y); // no need for now
-            _accordion.GetComponent<RectTransform>().SetParent(transform);
+            _accordionRectTransform = _accordion.GetComponent<RectTransform>();
+            _accordionRectTransform.SetParent(transform);
             _accordion.Setup(background, front);
-            _accordion.transform.position = position;
             _accordion.CreateAccordion(items);
         }
 
