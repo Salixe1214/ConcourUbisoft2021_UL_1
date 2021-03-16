c9eecfc030e4a29ee2945b452218d23adbe8e7ec
Simon <le6monstre@hotmail.com>
2021-03-16 16:46:10 -0400

    add again randompicker

--- ConcourUbisoft/Assets/Scripts/Doors/randomCodePicker.cs
+++ ConcourUbisoft/Assets/Scripts/Doors/randomCodePicker.cs
@@ -2,46 +2,63 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Doors;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class randomCodePicker : MonoBehaviour
 {
+    // Those are the colors possible for the syboles
+    // They're used mainly in switches
+    public enum SymbolColor
+    {
+        Blue,
+        Green,
+        Red,
+        Yellow,
+        Purple
+    }
 
-    private (DoorCode.Symbol, DoorCode.SymbolColor) _firstSymbol;
-    private (DoorCode.Symbol, DoorCode.SymbolColor) _secondSymbol;
+    // Those are the types of symbols, to make switches the easy way
+    public enum Symbol
+    {
+        One,
+        Two,
+        Three
+    }
+
+    private (Symbol, SymbolColor) _firstSymbol;
+    private (Symbol, SymbolColor) _secondSymbol;
     private List<DoorController.Direction> _sequence = new List<DoorController.Direction>();
-    
+
     // When the object awake, it randomly compose a combination of two different symbols of two different colors
     private void Awake()
     {
         Random.InitState(System.DateTime.Now.Millisecond);
         #region choosing Symbol
-        
+
         // Choice of the first symbol
-        var symbolValues = Enum.GetValues(typeof(DoorCode.Symbol)); // List of the symbols
+        var symbolValues = Enum.GetValues(typeof(Symbol)); // List of the symbols
         int firstSymbolIndex = Random.Range(0, symbolValues.Length); // Random index in this list
         Debug.Log("Index1: " + firstSymbolIndex);
-        DoorCode.Symbol randomSymbol1 = (DoorCode.Symbol) symbolValues.GetValue(firstSymbolIndex); // Expliciting this symbol
-        
+        Symbol randomSymbol1 = (Symbol)symbolValues.GetValue(firstSymbolIndex); // Expliciting this symbol
+
         // We can't choose this symbol again
         int secondSymbolIndex = firstSymbolIndex;
-        
+
         int infiniteLoopProtection = 1000;
         while (secondSymbolIndex == firstSymbolIndex)
         {
             secondSymbolIndex = Random.Range(0, symbolValues.Length);
-            
+
             // Te ensure there is no infinite loop
             infiniteLoopProtection = infiniteLoopProtection - 1;
-            if(infiniteLoopProtection <= 0)
+            if (infiniteLoopProtection <= 0)
             {
                 throw new Exception("An error occured while selecting the second Symbol");
             }
         }
-        
-        DoorCode.Symbol randomSymbol2 = (DoorCode.Symbol) symbolValues.GetValue(secondSymbolIndex);
+
+        Symbol randomSymbol2 = (Symbol)symbolValues.GetValue(secondSymbolIndex);
 
         #endregion
 
@@ -49,36 +66,36 @@ public class randomCodePicker : MonoBehaviour
         #region cgoosing Color
 
         // Choice of the first color
-        var colorValues = Enum.GetValues(typeof(DoorCode.SymbolColor)); // List of the colors
+        var colorValues = Enum.GetValues(typeof(SymbolColor)); // List of the colors
         int firstColorIndex = Random.Range(0, colorValues.Length); // Random index in this list
-        DoorCode.SymbolColor randomColor1 = (DoorCode.SymbolColor) colorValues.GetValue(firstColorIndex); // Expliciting this symbol
-        
+        SymbolColor randomColor1 = (SymbolColor)colorValues.GetValue(firstColorIndex); // Expliciting this symbol
+
         // We can't choose this color again
         int secondColorIndex = firstColorIndex;
-        
+
         infiniteLoopProtection = 1000;
         while (secondColorIndex == firstColorIndex)
         {
             secondColorIndex = Random.Range(0, colorValues.Length);
-            
+
             // Te ensure there is no infinite loop
             infiniteLoopProtection = infiniteLoopProtection - 1;
-            if(infiniteLoopProtection <= 0)
+            if (infiniteLoopProtection <= 0)
             {
                 throw new Exception("An error occured while selecting the second Symbol");
             }
         }
-        
-        DoorCode.SymbolColor randomColor2 = (DoorCode.SymbolColor) colorValues.GetValue(secondColorIndex);
+
+        SymbolColor randomColor2 = (SymbolColor)colorValues.GetValue(secondColorIndex);
 
         #endregion
-        
-        
+
+
         // Initializing the global variables
         _firstSymbol = (randomSymbol1, randomColor1);
         _secondSymbol = (randomSymbol2, randomColor2);
-        _sequence.AddRange(DoorCode.GetSymbolCode(_firstSymbol.Item2,_firstSymbol.Item1));
-        _sequence.AddRange(DoorCode.GetSymbolCode(_secondSymbol.Item2, _secondSymbol.Item1));
+        _sequence.AddRange(getSymbolCode(_firstSymbol.Item2, _firstSymbol.Item1));
+        _sequence.AddRange(getSymbolCode(_secondSymbol.Item2, _secondSymbol.Item1));
         string a = "";
         foreach (var i in _sequence)
         {
@@ -90,9 +107,139 @@ public class randomCodePicker : MonoBehaviour
 
     }
 
+    // This class return the sequence associated to a symbol and a color
+    private List<DoorController.Direction> getSymbolCode(SymbolColor pColor, Symbol pSymbol)
+    {
+        List<DoorController.Direction> dirList = new List<DoorController.Direction>();
+
+        switch (pColor)
+        {
+            case SymbolColor.Blue:
+                switch (pSymbol)
+                {
+                    case Symbol.One:
+                        dirList.Add(DoorController.Direction.Up);
+                        dirList.Add(DoorController.Direction.Up);
+                        dirList.Add(DoorController.Direction.Left);
+                        break;
+                    case Symbol.Two:
+                        dirList.Add(DoorController.Direction.Bottom);
+                        dirList.Add(DoorController.Direction.Bottom);
+                        dirList.Add(DoorController.Direction.Bottom);
+                        break;
+                    case Symbol.Three:
+                        dirList.Add(DoorController.Direction.Up);
+                        dirList.Add(DoorController.Direction.Left);
+                        dirList.Add(DoorController.Direction.Right);
+                        break;
+                    default:
+                        dirList.Clear();
+                        break;
+                }
+                break;
+            case SymbolColor.Green:
+                switch (pSymbol)
+                {
+                    case Symbol.One:
+                        dirList.Add(DoorController.Direction.Left);
+                        dirList.Add(DoorController.Direction.Left);
+                        dirList.Add(DoorController.Direction.Left);
+                        break;
+                    case Symbol.Two:
+                        dirList.Add(DoorController.Direction.Bottom);
+                        dirList.Add(DoorController.Direction.Up);
+                        dirList.Add(DoorController.Direction.Up);
+                        break;
+                    case Symbol.Three:
+                        dirList.Add(DoorController.Direction.Bottom);
+                        dirList.Add(DoorController.Direction.Bottom);
+                        dirList.Add(DoorController.Direction.Bottom);
+                        break;
+                    default:
+                        dirList.Clear();
+                        break;
+                }
+                break;
+            case SymbolColor.Red:
+                switch (pSymbol)
+                {
+                    case Symbol.One:
+                        dirList.Add(DoorController.Direction.Right);
+                        dirList.Add(DoorController.Direction.Left);
+                        dirList.Add(DoorController.Direction.Up);
+                        break;
+                    case Symbol.Two:
+                        dirList.Add(DoorController.Direction.Left);
+                        dirList.Add(DoorController.Direction.Right);
+                        dirList.Add(DoorController.Direction.Right);
+                        break;
+                    case Symbol.Three:
+                        dirList.Add(DoorController.Direction.Right);
+                        dirList.Add(DoorController.Direction.Up);
+                        dirList.Add(DoorController.Direction.Bottom);
+                        break;
+                    default:
+                        dirList.Clear();
+                        break;
+                }
+                break;
+            case SymbolColor.Yellow:
+                switch (pSymbol)
+                {
+                    case Symbol.One:
+                        dirList.Add(DoorController.Direction.Left);
+                        dirList.Add(DoorController.Direction.Right);
+                        dirList.Add(DoorController.Direction.Left);
+                        break;
+                    case Symbol.Two:
+                        dirList.Add(DoorController.Direction.Right);
+                        dirList.Add(DoorController.Direction.Right);
+                        dirList.Add(DoorController.Direction.Right);
+                        break;
+                    case Symbol.Three:
+                        dirList.Add(DoorController.Direction.Up);
+                        dirList.Add(DoorController.Direction.Left);
+                        dirList.Add(DoorController.Direction.Left);
+                        break;
+                    default:
+                        dirList.Clear();
+                        break;
+                }
+                break;
+            case SymbolColor.Purple:
+                switch (pSymbol)
+                {
+                    case Symbol.One:
+                        dirList.Add(DoorController.Direction.Up);
+                        dirList.Add(DoorController.Direction.Up);
+                        dirList.Add(DoorController.Direction.Right);
+                        break;
+                    case Symbol.Two:
+                        dirList.Add(DoorController.Direction.Right);
+                        dirList.Add(DoorController.Direction.Bottom);
+                        dirList.Add(DoorController.Direction.Right);
+                        break;
+                    case Symbol.Three:
+                        dirList.Add(DoorController.Direction.Bottom);
+                        dirList.Add(DoorController.Direction.Bottom);
+                        dirList.Add(DoorController.Direction.Up);
+                        break;
+                    default:
+                        dirList.Clear();
+                        break;
+                }
+                break;
+            default:
+                dirList.Clear();
+                break;
+        }
+
+        return dirList;
+    }
+
     #region getters
 
-    public ((DoorCode.Symbol, DoorCode.SymbolColor),(DoorCode.Symbol, DoorCode.SymbolColor)) GETSymbols()
+    public ((Symbol, SymbolColor), (Symbol, SymbolColor)) GETSymbols()
     {
         return (_firstSymbol, _secondSymbol);
     }
@@ -103,5 +250,5 @@ public class randomCodePicker : MonoBehaviour
     }
 
     #endregion
-    
+
 }
