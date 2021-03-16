f55cef9b5f0a57646718d99016be86571911f476
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2021-03-16 17:05:27 -0400

    Ajout de fichiers

---
+++ ConcourUbisoft/Assets/Scripts/Dialogs/DialogSystem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DialogSystem : MonoBehaviour
+{
+    [SerializeField] TextAsset rawText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
