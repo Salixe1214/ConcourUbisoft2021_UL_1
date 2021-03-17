66c89c488fb6276f35ba5d4a4c16d6997e96c872
Simon <le6monstre@hotmail.com>
2021-03-16 20:30:22 -0400

    add function to add list in tech ui

--- ConcourUbisoft/Assets/Scripts/Other/Level2Controller.cs
+++ ConcourUbisoft/Assets/Scripts/Other/Level2Controller.cs
@@ -11,17 +11,26 @@ public class Level2Controller : MonoBehaviour, LevelController
     [SerializeField] private GameObject[] _transportablesPrefab = null;
     [SerializeField] private SpawnObjectOnLineConveyor[] _spawners = null;
     [SerializeField] private FurnaceController _furnace = null;
+    [SerializeField] private InformationsSystem _techUI;
+    [SerializeField] private Sprite RobotHeadImage;
+    [SerializeField] private Sprite CrateImage;
+    [SerializeField] private Sprite GearImage;
+    [SerializeField] private Sprite BatteryImage;
+    [SerializeField] private Sprite PipeImage;
 
     public Color[] GetColors() => _possibleColors;
     public Color GetNextColorInSequence() => _furnace.GetNextColor();
     public int GetCurrentSequenceLenght() => _furnace.GetCurrentSequenceLenght();
     public TransportableType GetNextTypeInSequence() => _furnace.GetNextItemType();
 
+    private ImageLayout _imageList;
+    private List<Sprite> _itemSprites = new List<Sprite>();
+
     private void Start()
     {
         _furnace.GenerateNewColorSequences(_possibleColors);
         SpawnObjects();
-
+        _imageList = _techUI.GetList();
     }
 
     public void SpawnObjects()
@@ -56,5 +65,38 @@ public class Level2Controller : MonoBehaviour, LevelController
         GameObject randomPrefab = _transportablesPrefab[Random.Range(0, _transportablesPrefab.Length)];
         GameObject transportable = Instantiate(randomPrefab, solution.center, Quaternion.identity);
         transportable.gameObject.GetComponent<TransportableByConveyor>().Color = color; 
+    }
+
+    private void setItemsImageList()
+    {
+        _itemSprites.Clear();
+        FurnaceController.SequenceOfColor sequenceOfColor = _furnace.GetCurrentSequence();
+
+        for (int i = 0; i < _furnace.GetCurrentSequenceLenght(); i++)
+        {
+            TransportableType currentType = sequenceOfColor.types[i];
+            GameObject itemImage = new GameObject();
+            itemImage.AddComponent<Image>();
+
+            switch (currentType)
+            {
+                case TransportableType.RobotHead:
+                    _itemSprites.Add(RobotHeadImage);
+                    break;
+                case TransportableType.Crate:
+                    _itemSprites.Add(CrateImage);
+                    break;
+                case TransportableType.Gear:
+                    _itemSprites.Add(GearImage);
+                    break;
+                case TransportableType.Battery:
+                    _itemSprites.Add(BatteryImage);
+                    break;
+                case TransportableType.Pipe:
+                    _itemSprites.Add(PipeImage);
+                    break;
+            }
+        }
+        _imageList.CreateLayout(_itemSprites, sequenceOfColor.ColorsSequence);
     }
 }
