5319619698573940e036e31c08fe302d1ab3772e
Gabriel Bouchard <gaboutch@outlook.com>
2021-03-22 14:43:46 -0400

    Update ArmSound.cs

--- ConcourUbisoft/Assets/Scripts/RoboticArm/ArmSound.cs
+++ ConcourUbisoft/Assets/Scripts/RoboticArm/ArmSound.cs
@@ -6,36 +6,30 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class ArmSound : MonoBehaviour
 {
-    [SerializeField] private Transform target;
-    [SerializeField] private ArmController armController;
-    [SerializeField] private int smoothIterationCount = 10;
-    [SerializeField] private float volumeMultiplier = 1;
-    private Vector3 lastHeadPosition;
-    private AudioSource audioSource;
+	[SerializeField] private Transform target;
+	[SerializeField] private ArmController armController;
+	private Vector3 lastHeadPosition;
+	private AudioSource audioSource;
 
-    void Start()
-    {
-        lastHeadPosition = armController.Head.position;
+	void Start()
+	{
+		lastHeadPosition = armController.Head.position;
 
-        audioSource = GetComponent<AudioSource>();
-        audioSource.loop = true;
-    }
+		audioSource = GetComponent<AudioSource>();
+		audioSource.loop = true;
+	}
 
-    void LateUpdate()
-    {
-        float distance = Vector3.Distance( armController.Head.position, lastHeadPosition);
-        if (distance >= float.Epsilon)
-        {
-            float volume = distance / (armController.ControlSpeed * Time.deltaTime);
-            audioSource.volume =
-                (((audioSource.volume * smoothIterationCount) + volume) / (smoothIterationCount + 1)) *
-                volumeMultiplier;
-        }
-        else
-        {
-            audioSource.volume = 0;
-        }
-
-        lastHeadPosition =  armController.Head.position;
-    }
+	void LateUpdate()
+	{
+		float distance = Vector3.Distance(armController.Head.position, lastHeadPosition);
+		if (distance >= float.Epsilon)
+		{
+			audioSource.volume = distance / (armController.ControlSpeed * Time.deltaTime);
+		}
+		else
+		{
+			audioSource.volume = 0;
+		}
+		lastHeadPosition = armController.Head.position;
+	}
 }
