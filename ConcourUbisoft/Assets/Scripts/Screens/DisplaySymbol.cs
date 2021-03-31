8d0ac96cbf1be9b02388ac6a655a7b6d3ed6d0ac
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2021-03-31 12:16:59 -0400

    Door Code no colors

--- ConcourUbisoft/Assets/Scripts/Screens/DisplaySymbol.cs
+++ ConcourUbisoft/Assets/Scripts/Screens/DisplaySymbol.cs
@@ -7,13 +7,12 @@ public class DisplaySymbol : MonoBehaviour
 {
     [SerializeField] private List<Sprite> spritesList;
     [SerializeField] private DoorController door;
-    [SerializeField] private int idSymbol = 0; // Indicate if this shof the first (1) or second (2) symbol
+    [SerializeField] private int idSymbol = 0; // Indicate if this show the first (1) or second (2) symbol
     private Renderer _renderer = null;
 
-    private ((randomCodePicker.Symbol, randomCodePicker.SymbolColor),(randomCodePicker.Symbol, randomCodePicker.SymbolColor)) syms;
+    private (randomCodePicker.Symbol, randomCodePicker.Symbol) syms;
     private randomCodePicker.Symbol _symbol;
-    private randomCodePicker.SymbolColor _color;
-    private Color _rgbaColor;
+    private Color _rgbaColor = Color.white;
     
     void Start()
     {
@@ -23,42 +22,16 @@ public class DisplaySymbol : MonoBehaviour
         switch (idSymbol)
         {
             case 1:
-                _symbol = syms.Item1.Item1;
-                _color = syms.Item1.Item2;
+                _symbol = syms.Item1;
                 break;
             case 2:
-                _symbol = syms.Item2.Item1;
-                _color = syms.Item2.Item2;
+                _symbol = syms.Item2;
                 break;
             default:
                 throw new Exception("This pannel ID doesn't exist!\nChoices are: 1 or 2");
         }
 
         _renderer.material.mainTexture = spritesList[(int)_symbol].texture;
-
-        switch (_color)
-        {
-            case randomCodePicker.SymbolColor.Blue:
-                _rgbaColor = Color.blue;
-                break;
-            case randomCodePicker.SymbolColor.Green:
-                _rgbaColor = Color.green;
-                break;
-            case randomCodePicker.SymbolColor.Purple:
-                _rgbaColor = Color.magenta;
-                break;
-            case randomCodePicker.SymbolColor.Red:
-                _rgbaColor = Color.red;
-                break;
-            case randomCodePicker.SymbolColor.Yellow:
-                _rgbaColor = Color.yellow;
-                break;
-            default:
-                _rgbaColor = Color.black;
-                break;
-        }
-
-        _renderer.material.color = _rgbaColor;
     }
 
     // Update is called once per frame
