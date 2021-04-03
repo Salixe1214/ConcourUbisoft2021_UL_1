d91d2cab23b110022e34e1c4eb7a672d205a236b
Gabriel Bouchard <gaboutch@outlook.com>
2021-04-03 16:35:11 -0400

    fix LineConveyor

--- ConcourUbisoft/Assets/Scripts/Other/LineConveyor.cs
+++ ConcourUbisoft/Assets/Scripts/Other/LineConveyor.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class LineConveyor : Conveyor
 {
-    protected override void MoveObject(Rigidbody rigidbody)
-    {
-        Vector3 newPosition = rigidbody.position + this.transform.forward.normalized * Speed * Time.deltaTime;
-        //rigidbody.transform.LookAt(newPosition, Vector3.up);
-        rigidbody.transform.position = newPosition;
-    }
+	protected override void MoveObject(Rigidbody rigidbody)
+	{
+		Vector3 newPosition = rigidbody.position + this.transform.forward.normalized * Speed * Time.deltaTime;
+
+		rigidbody.MovePosition(newPosition);
+		//rigidbody.transform.LookAt(newPosition, Vector3.up);
+		//rigidbody.transform.position = newPosition;
+	}
 }
