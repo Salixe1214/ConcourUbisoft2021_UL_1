32ef76bb840b38e921dd85777af0545c01e735dc
mthb54 <42650980+mthb54@users.noreply.github.com>
2021-04-03 20:13:58 -0400

    Ending WIP

---
+++ ConcourUbisoft/Assets/Scripts/Other/Level3Controller.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+namespace Other
+{
+    public class Level3Controller :MonoBehaviour
+    {
+        private DialogSystem _dialogSystem;
+        private bool _endButtonPressed;
+        private void Start()
+        {
+            _dialogSystem = GameObject.FindGameObjectWithTag("DialogSystem").GetComponent<DialogSystem>();
+            _endButtonPressed = false;
+        }
+
+        public void StartLevel()
+        {
+            _dialogSystem.StartDialog("Area03_start");
+        }
+
+        public void OnButtonPressed()
+        {
+            if (!_endButtonPressed)
+            {
+                _endButtonPressed = true;
+                _dialogSystem.StartDialog("Area03_end");
+            }
+        }
+
+    }
+}
