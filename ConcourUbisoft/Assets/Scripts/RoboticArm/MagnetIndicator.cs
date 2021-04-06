2f1762676eb9215a1d191cfb62129a43e5c36b38
Gabriel Bouchard <gaboutch@outlook.com>
2021-04-06 10:46:45 -0400

    Magnet grab indicator

---
+++ ConcourUbisoft/Assets/Scripts/RoboticArm/MagnetIndicator.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Arm;
+using UnityEngine;
+
+public class MagnetIndicator : MonoBehaviour
+{
+	[SerializeField] private float emmisionIntensity = 10.0f;
+	[SerializeField] private Color grabbedColor = Color.green;
+	[SerializeField] private Color magnetActiveColor = Color.green;
+	[SerializeField] private Color magnetInactiveColor = Color.black;
+	[SerializeField] private MagnetController magnet;
+	private Renderer _renderer;
+
+	private void Start()
+	{
+		_renderer = GetComponent<Renderer>();
+	}
+
+	private void OnEnable()
+	{
+		magnet.OnMagnetActiveChange += HandleMagnetStateChange;
+		magnet.OnGrabStateChange += HandleMagnetStateChange;
+	}
+
+	private void OnDisable()
+	{
+		magnet.OnMagnetActiveChange -= HandleMagnetStateChange;
+		magnet.OnGrabStateChange -= HandleMagnetStateChange;
+	}
+
+	private void HandleMagnetStateChange()
+	{
+		if (magnet.MagnetActive)
+		{
+			if (magnet.Grabbed)
+			{
+				_renderer.material.color = grabbedColor;
+				_renderer.material.SetColor("_EmissionColor", grabbedColor * emmisionIntensity);
+				_renderer.material.EnableKeyword("_EMISSION");
+			}
+			else
+			{
+				_renderer.material.color = magnetActiveColor;
+				_renderer.material.SetColor("_EmissionColor", magnetActiveColor * emmisionIntensity);
+				_renderer.material.EnableKeyword("_EMISSION");
+			}
+		}
+		else
+		{
+			_renderer.material.color = magnetInactiveColor;
+			_renderer.material.SetColor("_EmissionColor", magnetInactiveColor * emmisionIntensity);
+			_renderer.material.DisableKeyword("_EMISSION");
+		}
+	}
+}
